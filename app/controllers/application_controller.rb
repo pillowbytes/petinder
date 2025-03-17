4b686366c3fb5a2b5156ccc1eea7a7a6a44db609
Guilherme Barbosa <lg.barbosa12@gmail.com>
2025-03-17 14:28:15 -0300

    Devise 100% funcional

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name])
+  end
+end
+
   # include Pundit::Authorization
 
   # # Pundit: allow-list approach
@@ -18,4 +27,3 @@ class ApplicationController < ActionController::Base
   # def skip_pundit?
   #   devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
   # end
-end
