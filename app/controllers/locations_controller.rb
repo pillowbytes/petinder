ac935b1a02a4f3649a914cc56b15ba6eedc683bc
Sidney Junior <siid.live@gmail.com>
2025-03-21 17:07:43 -0300

    Revert "abandoned branch"
    This reverts commit 7086904c60ea67c1ab5d145d3ffb2bd50f5b7d7e.

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,46 +1,15 @@
 class LocationsController < ApplicationController
   before_action :authenticate_user!
 
-  # def nearby
-  #   @current_pet = Pet.find_by(id: session[:current_pet_id])
-  #   @location = Location.includes(user: :pets).where.not(user: { pets: { id: nil } }) # Only locations with pets
-  #   @user_marker = { marker_html: render_to_string(partial: 'marker', locals: { pet: @current_pet }) }
-  #   @markers = @location.geocoded.map do |location|
-  #     {
-  #       lat: location.latitude,
-  #       lng: location.longitude,
-  #       marker_html: render_to_string(partial: 'marker', locals: { pet: location.user.pets.first })
-  #     }
-  #   end
-  # end
-
   def nearby
-    @current_pet = Pet.find_by(id: session[:current_pet_id])
-    @location = Location.includes(user: :pets).where.not(user: { pets: { id: nil } })
-
-    @geojson_markers = @location.geocoded.map do |location|
-      pet = location.user.pets.first
-      next unless pet
-
+    @location = Location.includes(user: :pets).where.not(user: { pets: { id: nil } }) # Only locations with pets
+    @user_marker = { marker_html: render_to_string(partial: 'marker', locals: { pet: current_user.pets.first }) }
+    @markers = @location.geocoded.map do |location|
       {
-        type: 'Feature',
-        geometry: {
-          type: 'Point',
-          coordinates: [location.longitude, location.latitude]
-        },
-        properties: {
-          name: pet.name,
-          photo_url: pet.photos.attached? ? cl_image_path(pet.photos.first.key, width: 80, height: 80, crop: "fill") : view_context.image_path("pet_placeholder.webp"),
-          is_current_user: pet.id == @current_pet&.id
-        }
+        lat: location.latitude,
+        lng: location.longitude,
+        marker_html: render_to_string(partial: 'marker', locals: { pet: location.user.pets.first })
       }
-    end.compact
-
-    @geojson_markers = {
-      type: 'FeatureCollection',
-      features: @geojson_markers
-    }
+    end
   end
-
-
 end
