3b0db4cd92720bcd1cafbb2f8e6773a5c63b3716
Sidney Junior <siid.live@gmail.com>
2025-03-23 07:45:09 -0300

    added map lwarm load to avoid slowness

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -2,8 +2,9 @@ class LocationsController < ApplicationController
   before_action :authenticate_user!
 
   def nearby
+    @current_pet = Pet.find_by(id: session[:current_pet_id])
     @location = Location.includes(user: :pets).where.not(user: { pets: { id: nil } }) # Only locations with pets
-    @user_marker = { marker_html: render_to_string(partial: 'marker', locals: { pet: current_user.pets.first }) }
+    @user_marker = { marker_html: render_to_string(partial: 'marker', locals: { pet: @current_pet }) }
 
     @markers = @location.geocoded.map do |location|
       {
